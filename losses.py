b2c5f0e9cec784581abbaa6fb9e76b61a454e54f
postBG <profile2697@gmail.com>
2020-05-10 17:54:38 +0900

    feat: add fcn

---
+++ losses.py
@@ -0,0 +1,12 @@
+import torch
+
+
+def dice_loss(pred: torch.Tensor, target: torch.Tensor, smooth=1.):
+    pred = pred.contiguous()
+    target = target.contiguous()
+
+    intersection = (pred * target).sum(dim=2).sum(dim=2)
+
+    loss = (1 - ((2. * intersection + smooth) / (pred.sum(dim=2).sum(dim=2) + target.sum(dim=2).sum(dim=2) + smooth)))
+
+    return loss.mean()
